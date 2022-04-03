084cd5177af31e3f7f9429841152cf9342f19cd4
Jay Bazuzi <jay@bazuzi.com>
2022-04-02 17:39:05 -0700

    switch to pyyml

--- tests/basic.py
+++ tests/basic.py
@@ -1,8 +1,6 @@
 #!/bin/env python3
 
-## PWP_REQUIRE: approvaltests==4.1.0
-import approvaltests.version
-
-assert approvaltests.version.version_number=="v4.1.0"
+## PWP_REQUIRE: pyyaml==6.0
+import pyyaml
 
 print("Applesauce") 
